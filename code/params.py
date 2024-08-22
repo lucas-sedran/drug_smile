209a8b7e4a4c1ce872b1353cb1d2bda94436d9fe
Dodoo Hellio <dorian.schnepp@gmail.com>
2024-08-22 16:43:43 +0200

    new params

--- code/params.py
+++ code/params.py
@@ -1,4 +1,10 @@
 import os
 
 ##################  VARIABLES  ##################
-RANDOM_STATE = os.environ.get(42)
+RANDOM_STATE = 42
+
+BUCKET_NAME = os.environ.get("BUCKET_NAME")
+MODEL_TARGET = os.environ.get("MODEL_TARGET")
+
+
+LOCAL_MODEL_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)),"code","model")
