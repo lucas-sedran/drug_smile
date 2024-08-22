e3b970f9560fda96f7ecff9bc5eb88a6c2247dbc
Lucas Sedran <lucas.sedran@hotmail.fr>
2024-08-22 14:26:38 +0200

    Creation registry.py

---
+++ code/_02_model_train/registry.py
@@ -0,0 +1,25 @@
+from code._01_preprocessing.vect_preproc import vect_load_data, vect_clean_data, vect_preprocess_data
+from code._02_model_train.vect_train import vect_split_data, vect_train_and_evaluate, vect_save_model
+from code.param import *
+
+def main_vecteurs(name_protein,nb_sample):
+    print(f"----- get_vecteurs_model {name_protein} {nb_sample} : START -----")
+    # Récupération et prétraitement des données
+    df = vect_load_data(name_protein,nb_sample)
+    df = vect_clean_data(df)
+    df = vect_preprocess_data(df)
+
+    # Division des données
+    X_train, X_val, y_train, y_val = vect_split_data(df)
+
+    # Entraînement et évaluation
+    best_model = vect_train_and_evaluate(X_train, X_val, y_train, y_val)
+
+    # Sauvegarde du meilleur modèle
+    vect_save_model(best_model, name_protein, nb_sample)
+    print(f"----- get_vecteurs_model {name_protein} {nb_sample} : STOP -----\n")
+
+if __name__ == "__main__":
+    nb_sample = NB_SAMPLE
+    name_protein = NAME_PROTEIN
+    main_vecteurs(name_protein=NAME_PROTEIN, nb_sample=NB_SAMPLE)
