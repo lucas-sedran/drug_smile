63f2160171350de38f52fb99413d5f85fceb99ec
Dodoo Hellio <dorian.schnepp@gmail.com>
2024-08-22 17:58:56 +0200

    load model from gcs to local

--- code/api/load_model.py
+++ code/api/load_model.py
@@ -9,22 +9,17 @@ def load_model():
 
         client = storage.Client()
         bucket = client.get_bucket(BUCKET_NAME)
-        print(f'*'*7)
-        print(bucket)
         blob = bucket.get_blob(blob_name ="model_vect_SVC_BRD4_10k.pkl")
-        print(f'#'*7)
-        print(blob.name)
+
 
         try:
             latest_model_path_to_save = os.path.join(LOCAL_MODEL_PATH, blob.name)
-            print(f'#'*7)
-            print(latest_model_path_to_save)
             blob.download_to_filename(latest_model_path_to_save)
-            print(f'#'*7)
-            print("here")
 
             #model = keras.models.load_model(latest_model_path_to_save)
+            return blob
             print("✅ model downloaded from cloud storage")
+
 
         except:
             print(f"\n❌ No model found in GCS bucket {BUCKET_NAME}")
