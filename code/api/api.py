b95108d62b1586aca30a231d0b2b1dcf97506a17
Dodoo Hellio <dorian.schnepp@gmail.com>
2024-08-26 11:56:28 +0200

    api post csv

--- code/api/api.py
+++ code/api/api.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, UploadFile
+#from tensorflow.keras.models import load_model
+import pandas as pd
+import io
 
 app = FastAPI()
 #app.state.model = load_model()
@@ -10,12 +12,29 @@ def home():
     return {"ping":"pong"}
 
 
+
 @app.get("/")
 def root():
     return {'greeting': 'Hello'}
 
+""" def load():
+    model_path = ""
+    model = load_model(model_path, compile=False)
+    return model
 
-@app.get("/predict")
-def predict(X_val):
-    print("here")
-    pass
+# Loading model
+model = load() """
+
+def preprocess_csv(csv_data):
+    data = pd.read_parquet(io.BytesIO(csv_data))
+    return data
+
+@app.post("/predict")
+async def predict(file : UploadFile):
+    csv = await file.read()
+
+    data = preprocess_csv(csv)
+    csv_dict = data.to_dict()
+    print(csv_dict)
+    #predict = model.predict(data)
+    return {"data":csv_dict}
