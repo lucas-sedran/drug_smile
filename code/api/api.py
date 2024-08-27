ebc9023e2321cd784b79cc24c4ee08af1f676731
Dodoo Hellio <dorian.schnepp@gmail.com>
2024-08-27 15:06:25 +0200

    load model implemented in api.py

--- code/api/api.py
+++ code/api/api.py
@@ -1,13 +1,11 @@
 from fastapi import FastAPI, UploadFile, Request,File,HTTPException,Form
-from code._01_preprocessing.vect_preproc import vect_preprocess_data
 import pandas as pd
 import io
 import joblib
-
+from code.params import *
+from google.cloud import storage
 
 app = FastAPI()
-#app.state.model = load_model()
-
 
 @app.get("/ping")
 def home():
@@ -19,18 +17,55 @@ def home():
 def root():
     return {'message': 'Hello'}
 
-def load():
-    global model
-    model_path = "/home/dodo/code/DodooHellio/drug_smile/models/model_vect_Logistic_Regression_BRD4_all.pkl"
+
+def load_model(name_file):
+
+    # Create folders models if not exist
+    if not os.path.isdir(MODELS_PATH):
+            os.makedirs(MODELS_PATH)
+    else:
+        pass
+
+    model_path = os.path.join(MODELS_PATH, name_file)
+    print(f'{model_path = }')
+
+    if not os.path.exists(os.path.join(MODELS_PATH, name_file)):
+
+        # Initialize Google Cloud Storage
+        storage_client = storage.Client(project=GCP_PROJECT)
+        bucket = storage_client.bucket(BUCKET_PROD_NAME)
+        blob = bucket.blob(name_file)
+        try:
+            blob.download_to_filename(model_path)
+            print("✅ model downloaded from cloud storage")
+        except:
+            print(f"\n❌ No {name_file} found in GCS bucket {BUCKET_PROD_NAME} ❌")
+            os.remove(model_path)
+
+            return None
+    else :
+        print("Model already in local folder")
+        pass
     try:
         model = joblib.load(model_path)
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Unable to load model: {e}")
+        raise HTTPException(status_code=500, detail=f" ### Unable to load model {type}: {e} ###")
     return model
 
 
-# Loading model
-model = load()
+
+@app.on_event("startup")
+async def startup_event():
+    print("*"*50)
+    print(" Startup API =) ")
+    print("*"*50)
+
+    name_file = "model_vect_Logistic_Regression_BRD4_all.pkl"
+    app.state.model_vect_Logistic_Regression_BRD4_all = load_model(name_file)
+
+    name_file = "model_GNN_BRD4_all.pkl"
+    app.state.model_GNN_BRD4_all = load_model(name_file)
+
 
 
 @app.post("/predict")
@@ -42,13 +77,19 @@ async def predict( model_name: str = Form(...) ,file : UploadFile = File(...)):
 
     print(model_name)
     if model_name == "Logistic Regression":
+        model = app.state.model_vect_Logistic_Regression_BRD4_all
         preproc_df = vect_preprocess_data(df)
         X = preproc_df['ecfp'].tolist()
         prediction = model.predict(X)
         return prediction
 
     if model_name == "Random Forest":
-        return { "message" : "RF"}
+        return { "message" : "From API : Random Forest"}
+
+
+    if model_name == "GNN":
+        model = app.state.model_GNN_BRD4_all
+        return { "message" : "From API : GNN Loaded !!!!!"}
 
 
     return {"message":"Parquet received","columns": df.columns.tolist(),"model_selected":model_name}
