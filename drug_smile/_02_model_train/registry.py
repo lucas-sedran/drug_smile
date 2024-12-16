b8b555130c874d536fcc6b51ac22e0433401b7e0
Lucas Sedran <lucas.sedran@hotmail.fr>
2024-12-16 18:46:34 +0100

    Suppression de la sauvegarde du modèle dans main_vecteurs car elle est déjà faite dans la sous-fonction d'avant, dans vect_train_and_evaluate

--- drug_smile/_02_model_train/registry.py
+++ drug_smile/_02_model_train/registry.py
@@ -30,11 +30,9 @@ def main_vecteurs(name_protein,nb_sample):
     # Division des données
     X_train, X_val, y_train, y_val = vect_split_data(df)
 
-    # Entraînement et évaluation
+    # Entraînement, évaluation et sauvegarde
     best_model, name_model = vect_train_and_evaluate(NAME_VECT_MODEL, X_train, X_val, y_train, y_val)
 
-    # Sauvegarde du meilleur modèle
-    vect_save_model(name_model, best_model, name_protein, nb_sample)
     print(f"----- get_vecteurs_model {name_protein} {nb_sample} : STOP -----\n")
 
 def main_GNN(name_protein,nb_sample):
